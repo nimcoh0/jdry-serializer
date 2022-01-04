3fb41764f9d1fc8e456e88ef6f52a114bd2f2dac
nimcoh <nimcoh0@gmail.com>
2022-01-04 12:17:10 +0200

    update

--- src/main/java/org/softauto/serializer/service/MessageType.java
+++ src/main/java/org/softauto/serializer/service/MessageType.java
@@ -8,7 +8,7 @@ public enum MessageType {
 
     public static MessageType fromString(String text) {
         for (MessageType b : MessageType.values()) {
-            if (b.name().toLowerCase().equals(text)) {
+            if (b.name().toLowerCase().equals(text) || b.name().equals(text)) {
                 return b;
             }
         }
